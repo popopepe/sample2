394453d0505de8dcf2c2ec2f2843279eddcfd658
popopepe <simon@callbus.com>
2017-01-26 21:20:41 +0900

    test sample2

---
+++ src/components/DataProbs.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>probs data</h1>
+    <span>{{ message }}</span>
+  </div>
+  
+</template>
+
+<script>
+  module.exports = {
+    props: ['message'],
+  };
+</script>
+
